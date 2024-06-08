4623a58a394a21ae71fb0746b6d6df67b48dfe3d
suisan0731 <suisan0731@gmail.com>
2024-06-08 18:47:19 +0900

    prepare for deploy

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,22 @@
+#指定したイメージの内容を持ってくる(1)
 FROM python:3.11
-WORKDIR /bot
-COPY . /bot
+
+#Dockerコンテナのカレントディレクトリ設定
+WORKDIR /usr/src/app
+
+#requirements.txtをコンテナ側にコピー
+COPY requirements.txt /usr/src/app/
+
+#Dockerイメージ作成時に実行(2)
 RUN python3 -m pip install -r requirements.txt
-CMD python3 main.py
+
+#(3)
+COPY . /usr/src/app/
+
+#コンテナを起動のタイミングで実行
+CMD ["python3", "main.py"]
+
+
+#python:3.11のイメージを元に(1)
+#ライブラリをインストールし(2)
+#最後に必要なソースコードをコンテナイメージにコピーしディスクイメージを作成(3)
--- bot/main.py
+++ bot/main.py
@@ -1,17 +1,14 @@
 import discord
 from discord.ext import commands
 import os
-from dotenv import load_dotenv
 
-#envファイル読み込み
-load_dotenv()
-
-#環境変数を変数に読み込み
-TOKEN = os.environ['TOKEN']
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
+
+#環境変数を変数に読み込み
+TOKEN = os.environ['TOKEN']
 
 #送信するテキストチャンネル指定
 send_to_channel = int(os.environ["SEND_CHANNEL"])
@@ -20,28 +17,27 @@ send_to_channel = int(os.environ["SEND_CHANNEL"])
 my_guild = int(os.environ["MY_GUILD"])
 
 
+#誰かがボイスチャンネルに参加したら通知される非同期関数
 @bot.event
 async def on_voice_state_update(member, before, after):
 
     #入退室に関係ない処理をスルー and 該当ギルドだけ処理をする
     if before.channel != after.channel and my_guild == member.guild.id:
-        print("enter or leave")
+        print("on_voice_state_update function invoked")
 
         #入室
         if before.channel is None:
             print(f"{member.name} left from {after.channel}")
-            message = f"```{member.name}が入室しました。```"
+            message = f"```{member.name} が入室しました。```"
         
         #退室
         if after.channel is None:
             print(f"{member.name} left from {before.channel}")
-            message = f"```{member.name}が退室しました。```"
-        
+            message = f"```{member.name} が退室しました。```"
+
         #メッセージを送信
         for channel in bot.get_all_channels():
-            print(channel.id)
             if channel.id == send_to_channel:
-                print("send message")
                 await channel.send(message)
 
 
